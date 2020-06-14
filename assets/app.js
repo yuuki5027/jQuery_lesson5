7e773e20bbd4deb7380aab83f9382895d4672db0
西山優樹 <n.yuuki5027@gmail.com>
2020-06-14 13:53:57 +0900

    続き

--- assets/app.js
+++ assets/app.js
@@ -15,5 +15,37 @@ $(function() {
 
   // ツールチップの記述
   $(".tooltip").tooltipster();
-});
 
+  // ドロップダウンメニューの記述
+  $("ul.sf-menu").superfish();
+  $("ul.sf-menu a").click(function() {
+    return false;
+  });
+
+  // スクロールイベントの記述
+  $("#menu").waypoint(function() {
+    $("header").toggleClass("fix");
+  });
+  $("#menu1").waypoint(function() {
+    $("header ul li").removeClass("recent");
+    $("header ul li:nth-child(1)").addClass("recent");
+  });
+  $("#menu2").waypoint(function() {
+    $("header ul li").removeClass("recent");
+    $("header ul li:nth-child(2)").addClass("recent");
+  });
+  $("#menu3").waypoint(function() {
+    $("header ul li").removeClass("recent");
+    $("header ul li:nth-child(3)").addClass("recent");
+  });
+  $("#menu4").waypoint(function() {
+    $("header ul li").removeClass("recent");
+    $("header ul li:nth-child(4)").addClass("recent");
+  });
+
+  // svgの記述
+  var $svg = $("svg").drawsvg();
+  $("#menu3").waypoint(function() {
+    $svg.drawsvg("animate");
+  });
+});
